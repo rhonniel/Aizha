3dbc3d6881baaf54af371fc051676afd08d479d9
unknown <rhonniel777@gmail.com>
2021-02-08 22:28:04 -0400

    firts commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] Paddle paddle;
+    [SerializeField] float startForceX;
+    [SerializeField] float startForceY;
+
+    Vector2 paddleToBallVector;
+    bool hasStarted;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        paddleToBallVector = transform.position - paddle.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /* El metodo que lookea la bola con la barra no dejara que esta se lance al aejecutarse 
+        milisegundos despues, por esto se coloca una condicion al igual tampoco se desea que 
+        el jugador continua lanzando la bola mas de una vez */
+        if (!hasStarted) 
+        {
+            LookBallToPaddle();
+            LaunchOnMouseClick();
+           
+        }
+     
+        
+        
+    }
+
+    private void LookBallToPaddle()
+    {
+        Vector2 paddlePos = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
+        transform.position = paddlePos + paddleToBallVector;
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        // el indice cero significa el click izquierdo
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            /*El componente Rigibody es capas de tomar un vector de velocidad, 
+             * a nivel fisico es logico que sea un cuerpo rigido quien actue de esta forma y no el transform.
+             al utilizar un cuerpo rigido el objeto funcionara bajo las leyes de la fisica, 
+            este vector de velocidad es la fuerza que lo impulsa a esa direccion dada*/
+            GetComponent<Rigidbody2D>().velocity = new Vector2(startForceX, startForceY);
+        }
+    }
+
+
+
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour  
+{
+    //al serializar el campo este se mantiene privado pero se muestra en la interfaz de unity
+    [SerializeField] float unitsScreen = 16;
+    [SerializeField] float minUnits = 0;
+    [SerializeField] float maxUnits = 14;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //screen.width es 1080 en este caso, la cantidad de pixeles, al dividir su movimiento en estos y multiplicarlo por la unidad en que se distribuyen los pixeles en la pantalla
+        // se obtiene un valor de unidad de 0 hasta al numero de las unidades de la oantalla (16) por ejemplo
+        float move = Mathf.Clamp((Input.mousePosition.x / Screen.width * unitsScreen), minUnits, maxUnits);
+        Vector2 paddlePos = new Vector2(move, transform.position.y);
+        transform.position = paddlePos;
+    }
+}
